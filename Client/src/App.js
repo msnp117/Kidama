2c53e154de482bf85f3f27663f66eb75a793624d
msnp117 <solemza530@gmail.com>
2022-08-02 15:34:37 -0300

    Button Login

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,30 +1,26 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
-
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-
-import Home from './pages/Home'
-import LetraPage from './pages/LetraPage';
-import AbcPage from './pages/AbcPage';
+import Home from "./pages/Home";
+import LetraPage from "./pages/LetraPage";
+import AbcPage from "./pages/AbcPage";
 
 function App() {
   return (
     <BrowserRouter>
-      <Navbar/>
+      <Navbar />
 
-     <main className='main text-center'>
+      <main className="main text-center">
         <Routes>
-
-          <Route path='/' element={<Home/>} />
-          <Route path='/abc' element={<AbcPage/>} />
-          <Route path='/abc/:letra' element={<LetraPage/>} />
-
+          <Route path="/" element={<Home />} />
+          <Route path="/abc" element={<AbcPage />} />
+          <Route path="/abc/:letra" element={<LetraPage />} />
         </Routes>
-     </main>
+      </main>
 
-      <Footer/>
+      <Footer />
     </BrowserRouter>
   );
 }
--- Client/src/pages/Home.js
+++ Client/src/pages/Home.js
@@ -1,21 +1,19 @@
-
-
 export default function Home() {
-    return(
-		<div className="p-4">
-			
-			<div className="cuadro-de-texto d-flex align-items-center m-auto bg-white">
-				<p>
-					¡Hola!<br/>
-					Bienvenido a Small Helps, yo soy Manchas <br/>
-					Aqui puedes elegir lo que deseas aprender miau 
-				</p>
-			</div>
-			<img className="pt-5"
-				src="https://cdn.pixabay.com/photo/2020/11/15/18/51/cat-5746875_960_720.png" alt=""/>
-
-
-
-		</div>	
-    )
+  return (
+    <div className="p-4">
+      <div className="cuadro-de-texto d-flex align-items-center m-auto bg-white">
+        <p>
+          ¡Hola!
+          <br />
+          Bienvenido a Small Helps, yo soy Manchas <br />
+          Aqui puedes elegir lo que deseas aprender miau
+        </p>
+      </div>
+      <img
+        className="pt-5"
+        src="https://cdn.pixabay.com/photo/2020/11/15/18/51/cat-5746875_960_720.png"
+        alt=""
+      />
+    </div>
+  );
 }
